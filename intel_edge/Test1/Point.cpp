65ca46c19c7396b659abf8b1483a8e223236c6fb
EMBEDDED <kyc960904@naver.com>
2023-11-09 12:11:21 +0900

    1

--- intel_edge/Test1/Point.cpp
+++ intel_edge/Test1/Point.cpp
@@ -17,6 +17,10 @@ Point& Point::SetP(int x, int y)
 	this->x = x; this->y = y; // 'this' 셀프포인터 사용. this -> x 의 x는 맴버변수를 지칭
 	return *this;
 }
+Point& operator+(Point p) // 두점 curP와 p의 +연산결과를 (새로운 Point로) 반환
+{
+
+}
 Point& Point::ViewP(const char *s) // NULL은 프로토타입에서만 정의
 {
 	printf("%s(%d, %d)\n", s, x, y);
@@ -25,5 +29,10 @@ Point& Point::ViewP(const char *s) // NULL은 프로토타입에서만 정의
 Point& Point::MoveP(int dx, int dy)
 {
 	this->x = x + dx; this->y = y + dy; // this-> x += dx; this-> += dy
+	return *this;
+}
+Point3D& Point3D::SetP(Point3D p)
+{
+	X() = p.X(); Y() = p.Y(); z = p.z;
 	return *this;
 }
--- intel_edge/Test1/Point.h
+++ intel_edge/Test1/Point.h
@@ -4,22 +4,22 @@
 class Point
 {
 private: // default이긴 하나, 명시적으로 표시함.
-	int x;
 protected:
+	int x;
 	int y;
 public:
-
 	Point(int x1 = 0, int y1 = 0) : x(x1), y(y1)// 생성자 형성 (객체화 해주기 위해서 실시)
 	{
 	}
 	Point& SetP(Point P); // self-reference
 	Point& SetP(int x, int y);
-	double Dist(Point p); // 함수호출
+	double Dist(Point p); // Distance, 함수호출
 	Point& ViewP(const char* s = NULL); // View Current Point// NULL은 프로토타입에서만 정의
 	Point& MoveP(int off_x, int off_y); // Move Point Position 
 	int& X() { return x; }; // private 영역에 맴버변수가 있을 경우, 외부영역에서 맴버변수를 확인하는 함수(단방향 - 값을 수정할 수 없음)
 	int& Y() { return y; };
 	//int& X1() { return x; }; // 참조를 활용하여 private 영역에 있는 맴범변수 수정하게 해줌.(양방향)
+	Point& operator+(Point p);
 };
 
 class Point3D : public Point
@@ -29,9 +29,10 @@ private:
 public:
 	Point3D(int x = 0, int y = 0, int z = 0) : Point(x, y), z(z) // 상위클래스의 생성자 호출
 	{}
+	Point3D& SetP(Point3D p);
 	double Dist3D(Point3D p) // 3D 거리측정함수
 	{
-		int w = X() - p.X();
+		int w = X() - p.X(); // x, y가 private 영역일 때 이 방법으로 호출
 		int h = y-p.y;
 		int d = z - p.z;
 
